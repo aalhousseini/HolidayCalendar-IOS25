31e00447767f62ee871fd0459843ca7e4928cbfc
hanno <hanno.wiesen@rwth-aachen.de>
2025-01-19 13:23:01 +0100

    export + import + notifications

---
+++ Calendar/Models/DoorCodable.swift
@@ -0,0 +1,38 @@
+//
+//  CalendarDOOR.swift
+//  Holiday Calendar
+//
+//  Created by Al Housseini, Ahmad on 10.01.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class DoorModel: Identifiable {
+    @Attribute(.unique) var id: UUID = UUID()
+    var number: Int
+    var unlockDate: Date
+    
+    var isCompleted: Bool
+    
+    var isLocked: Bool {
+        return unlockDate > Date()
+    }
+    
+    var quote: String?
+    var image: Data?
+    var challenge: String
+    
+    @Relationship(inverse: \CalendarModel.doors) var calendar: CalendarModel?
+    
+    init(number: Int, unlockDate: Date, quote: String? = nil, image: Data? = nil, challenge: String, calendar: CalendarModel? = nil) {
+        self.number = number
+        self.unlockDate = unlockDate
+        self.quote = quote
+        self.image = image
+        self.challenge = challenge
+        self.calendar = calendar
+        self.isCompleted = false
+    }
+}
--- Calendar/Utils/Utils.swift
+++ Calendar/Utils/Utils.swift
@@ -48,22 +48,24 @@ func checkNotificationSettings() {
     }
 }
 
-func scheduleTestNotification() {
+func createDoorNotification(door: Door) -> Void {
     let content = UNMutableNotificationContent()
-    content.title = "Test Notification"
-    content.body = "This is a test notification."
+    content.title = "New Door"
+    content.body = "You can open a new Door in \(door.calendar?.name ?? "a calendar")"
     content.sound = .default
+    
+    let triggerDate = Calendar.current.date(bySettingHour: 10, minute: 0, second: 0, of: door.unlockDate) ?? door.unlockDate
+    let triggerDateComponents = Calendar.current.dateComponents([.hour, .minute, .day, .month], from:  triggerDate)
 
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-    let request = UNNotificationRequest(identifier: "testNotification", content: content, trigger: trigger)
+    let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponents, repeats: false)
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
 
     UNUserNotificationCenter.current().add(request) { error in
         DispatchQueue.main.async {
             if let error = error {
                 print("Error scheduling notification: \(error)")
             } else {
-                print("Notification scheduled.")
+                print("Notification scheduled for \(triggerDate).")
             }
         }
     }
